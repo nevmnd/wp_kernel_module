f0b353de393f834162adb7ff67889d254b0d26e5
work <danil.chest@gmail.com>
2018-12-16 20:50:44 +0500

    added reading from sysfs

--- watchpoint.c
+++ watchpoint.c
@@ -2,19 +2,51 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>         /* Needed for the macros */
 
-static long int address = 0;
+static int address = 0;
 
-module_param(address, long, 0);
+module_param(address, int, 0);
+
+static ssize_t store_value(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+    kstrtoint(buf, 10, &address);
+    printk(KERN_INFO "Watchpoint address from /sys/kernel/wp_addr/counter: 0x%u\n", address);
+    return count;
+}
+
+// register function to attribute
+static struct kobj_attribute wp_attribute = __ATTR(counter, 0664, NULL, store_value);
+
+// put attribute to attribute group
+static struct attribute *attrs[] = {
+    &wp_attribute.attr,
+    NULL,   /* NULL terminate the list*/
+};
+static struct attribute_group  reg_attr_group = {
+    .attrs = attrs,
+};
+
+static struct kobject *wp_kobj;
 
 static int __init wp_init(void)
 {
 	printk(KERN_INFO "Loading watchpoint module...\n");
-	printk(KERN_INFO "Watchpoint address: 0x%lX\n", address);
+	if (address)
+            printk(KERN_INFO "Watchpoint address from module params: 0x%u\n", address);
+        else {
+            wp_kobj = kobject_create_and_add("wp_addr", kernel_kobj);
+            if (!wp_kobj)
+                return -ENOMEM;
+            if(sysfs_create_group(wp_kobj, &reg_attr_group)){
+                kobject_put(wp_kobj);
+                return -ENOMEM;
+            }
+        }
 	return 0;
 }
 
 static void __exit wp_exit(void)
 {
+	kobject_put(wp_kobj);
 	printk(KERN_INFO "Unloading watchpoint...\n");
 
 }
