551f1aab58931094dd264625dca9b6891090bc54
work <danil.chest@gmail.com>
2018-12-19 00:52:56 +0500

    moved main cycle to thread

--- test_wp.c
+++ test_wp.c
@@ -2,59 +2,45 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>         /* Needed for the macros */
 #include <linux/delay.h>
+#include <linux/kthread.h>
 
-static short int use_sys = 0, counter = 0, i = 0;
+static unsigned int counter = 0;
+static struct task_struct *test_thread;
 
-module_param(use_sys, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-
-// function for many symbol data enter
-static ssize_t show_value(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static int test_handler(void *data)
 {
-	return sprintf(buf, "%d\n", counter);
+    while (counter < 5) {
+        counter++;
+        msleep (10000);
+        printk(KERN_INFO "Counter is: %u\n", counter);
+    };
+    return 0;
 }
 
-// register function to attribute
-static struct kobj_attribute test_attribute = __ATTR(counter, 0664, show_value, NULL);
-
-// put attribute to attribute group
-static struct attribute *attrs[] = {
-    &test_attribute.attr,
-    NULL,   /* NULL terminate the list*/
-};
-static struct attribute_group  reg_attr_group = {
-    .attrs = attrs,
-};
-
-static struct kobject *test_kobj;
 
 static int __init test_init(void)
 {
-	printk(KERN_INFO "Loading test_wp module...\n");
-        if (use_sys)
-            printk(KERN_INFO "Flag use_sys is set. Variable address will be stored in /sys/kernel/wp_addr\n");
-        test_kobj = kobject_create_and_add("wp_addr", kernel_kobj);
-        if (!test_kobj)
-            return -ENOMEM;
-        if(sysfs_create_group(test_kobj, &reg_attr_group)){
-            kobject_put(test_kobj);
-            return -ENOMEM;
+    int err;
+	
+    printk(KERN_INFO "Loading test_wp module...\n");
+    //probably %p
+    printk(KERN_INFO "Counter address: 0x%px\n", &counter);
+
+    test_thread = kthread_create(test_handler, NULL, "test_handler");
+    if(IS_ERR(test_thread)){ 
+        printk("Unable to start kernel thread\n");
+        err = PTR_ERR(test_thread);
+        test_thread = NULL;
+        return err;
         }
-        //probably %p
-        printk(KERN_INFO "Counter address: 0x%px\n", &counter);
-        while (counter < 3) {
-            counter++;
-            msleep (10000);
-            printk(KERN_INFO "Counter is: %d\n", counter);
-            msleep (10000);
-        };
-	return 0;
+    wake_up_process(test_thread);
+
+    return 0;
 }
 
 static void __exit test_exit(void)
 {
 	printk(KERN_INFO "Unloading test_wp...\n");
-        kobject_put(test_kobj);
-
 }
 
 module_init(test_init);
